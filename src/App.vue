25e2da7b0d8ed1b295e02750d522fee41b8b5e98
ishizuka takeaki <>
2022-12-19 17:15:58 +0900

    eslint & prettier setting_2

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<template>
-    <div class="main container px-0">
-        <BackImageContainer>
-            <carousel class="mainCarousel" dir="rtl">
-                <slide class="mainCarousel__view" v-for="slide in munes" :key="slide.name">
-                    <component :is="slide.component"></component>
-                </slide>
-
-                <template #addons="{ currentSlide }">
-                    <h1>{{currentSlide}}</h1>
-                    <pagination />
-                </template>
-            </carousel>
-        </BackImageContainer>
-    </div>
-</template>
-
 <script setup lang="ts">
-import 'vue3-carousel/dist/carousel.css';
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
-import BackImageContainer from './components/BackImageContainer.vue'
-import {ref, shallowRef} from "vue";
-import HomeSlide from "./components/homeSlide.vue"
-import StorySlide from "./components/StorySlide.vue"
-const homeSlide = shallowRef(HomeSlide)
-const storySlide = shallowRef(StorySlide)
-interface IMenu {
-    name: string;
-    component: any;
-}
-const munes = ref<IMenu[]>([
-    {name: '記念日', component: homeSlide},
-    {name: 'ストーリー', component: storySlide}
-])
+import ClickCounter from './components/ClickCounter.vue'
 </script>
+
+<template>
+  <div class="main container px-0">
+    <h1>APP.vue</h1>
+    <ClickCounter />
+  </div>
+</template>
 
 <style scoped lang="scss">
 .main {
-
-    .mainCarousel {
-
-        &__view {
-            height: 100vh;
-        }
+  .mainCarousel {
+    &__view {
+      height: 100vh;
     }
+  }
 }
 </style>
--- src/components/ClickCounter.vue
+++ src/components/ClickCounter.vue
@@ -6,47 +6,55 @@ const innerCount = ref<number>(0)
 
 // props
 const props = withDefaults(
-    defineProps<{
-        modelValue: number
-    }>(),
-    {
-        modelValue: 0,
-    }
+  defineProps<{
+    modelValue: number
+  }>(),
+  {
+    modelValue: 0,
+  }
 )
 // emit
 const emits = defineEmits<{
-    (e: 'update:modelValue', count: number): void
+  (e: 'update:modelValue', count: number): void
 }>()
 
 onMounted(() => {
-    innerCount.value = props.modelValue
+  innerCount.value = props.modelValue
 })
 
 // methods
 const countUP = () => {
-    emits('update:modelValue', innerCount.value++)
+  emits('update:modelValue', innerCount.value++)
 }
 const countDown = () => {
-    emits('update:modelValue', innerCount.value--)
+  emits('update:modelValue', innerCount.value--)
 }
 </script>
 
 <template>
-    <div class="card">
-        <h1>{{ innerCount }}</h1>
-        <button type="button" class="btn btn-success" @click="countUP">
-            +
-        </button>
-        <button type="button" class="btn btn-danger" @click="countDown">
-            -
-        </button>
-    </div>
+  <div class="card">
+    <h1>{{ innerCount }}</h1>
+    <button
+      type="button"
+      class="btn btn-success"
+      @click="countUP"
+    >
+      +
+    </button>
+    <button
+      type="button"
+      class="btn btn-danger"
+      @click="countDown"
+    >
+      -
+    </button>
+  </div>
 </template>
 
 <style lang="scss">
 .card {
-    button {
-        font-size: 20px;
-    }
+  button {
+    font-size: 20px;
+  }
 }
 </style>
